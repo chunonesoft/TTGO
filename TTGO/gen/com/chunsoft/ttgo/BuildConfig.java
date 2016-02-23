a627b2643eb62279edf22870df04eeaca5f6cff5
chunonesoft <chun_soft@qq.com>
2016-02-23 22:26:47 +0800

    TTGO项目2016/2/23

---
+++ TTGO/gen/com/chunsoft/ttgo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chunsoft.ttgo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TTGO/src/com/chunsoft/ttgo/home/Main_FA.java
@@ -0,0 +1,13 @@
+package com.chunsoft.ttgo.home;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class Main_FA extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+	}
+
+}
